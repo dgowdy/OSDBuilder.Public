af16d7bf0db7d2c0d0be025bd5fd48d1b2bab292
David Gowdy <34044429+dgowdy@users.noreply.github.com>
2019-05-10 13:29:03 -0400

    Vivid changes

--- Content/Scripts/Global Set-TimeZone EasternStandardTime.ps1
+++ Content/Scripts/Global Set-TimeZone EasternStandardTime.ps1
@@ -2,14 +2,14 @@
 #   http://osdeploy.com
 #
 #   OSDBuilder Script
-#   Global Set-TimeZone CentralStandardTime.ps1
+#   Global Set-TimeZone EasternStandardTime.ps1
 #   Version 19.3.15
 #
 #   Displays Variables and Prompts for Enter
-#   Set Time Zone to Central Standard Time for Install.wim
+#   Set Time Zone to Eastern Standard Time for Install.wim
 #======================================================================================
 if (Test-Path $MountDirectory) {
-    Dism /Image:"$MountDirectory" /Set-TimeZone:"Central Standard Time" /LogPath:"$Logs\$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Dism-SetTimeZone.log"
+    Dism /Image:"$MountDirectory" /Set-TimeZone:"Eastern Standard Time" /LogPath:"$Logs\$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Dism-SetTimeZone.log"
 }
 #======================================================================================
 #   Testing
